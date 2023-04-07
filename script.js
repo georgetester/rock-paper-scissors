71f0a616eaf3c4d33adc6c13023189fe8bb7a559
georgetester <georgemtester@gmail.com>
2023-04-07 23:38:49 +0100

    Moved variable declarations around to make score update accurately

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
-let score = playerScore + ' : ' + computerScore
+
 
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let computerSelection = choices[Math.floor(3 * Math.random())];
-
     return computerSelection;
 }
 
@@ -20,19 +19,22 @@ function playRound() {
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
     (playerSelection == 'scissors' && computerSelection == 'paper') ||
     (playerSelection == 'paper' && computerSelection == 'rock')) {
-        playerScore ++;
+        playerScore = playerScore +1;
         result = `You win! ${playerSelection} beats ${computerSelection}`;
         console.log(result);
+        let score = playerScore + ' : ' + computerScore;
         console.log(`The score is ${score}`)
 
     } else if (computerSelection == playerSelection) {
         result = 'It\'s a draw!';
         console.log(result);
+        let score = playerScore + ' : ' + computerScore;
         console.log(`The score is ${score}`)
     } else {
-        computerScore ++;
+        computerScore = computerScore +1;
         result = `You lose! ${computerSelection} beats ${playerSelection}`;
         console.log(result);
+        let score = playerScore + ' : ' + computerScore;
         console.log(`The score is ${score}`);
     }
 }
@@ -46,10 +48,13 @@ function playGame() {
     };
 
     if (playerScore = 5) {
+        let score = playerScore + ' : ' + computerScore;
         console.log(`You win the game! The score was ${score}`)
     } else {
+        let score = playerScore + ' : ' + computerScore;
         console.log(`You lose the game you utter noob! The score was ${score}`)
     }
 }
+
 
 playGame();
